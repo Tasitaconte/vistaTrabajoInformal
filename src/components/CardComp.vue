712a78a19e0439d0d78f1dc08d60266d07c6418a
thomas rodriguez <thomasmauricio521@gmail.com>
2022-11-24 05:24:27 -0500

    first commit

---
+++ src/components/CardComp.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="card">
+		<div class="card-body">
+	
+			<h1>{{title}}</h1>
+			<slot></slot>
+			<div class="d-grid gap-2">
+				<button
+					v-for="btn in btns"
+					:key="btn.txt"
+					class="btn"
+					:class="btn.class">
+					{{ btn.txt }}
+				</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'CardComp',
+		props: ['title', 'parrafo', 'btns'],
+	}
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="home">
+    <div class="row">
+      <div class="col-lg-6 text-center">
+        <h1>App Medts</h1>
+        <p class="" style="text-align: justify">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ab
+          quibusdam obcaecati odio ut nihil laboriosam commodi nulla asperiores
+          temporibus cumque iure, quod neque beatae eveniet, ducimus eligendi
+          autem earum.
+        </p>
+      </div>
+      <div class="col-lg-6">
+        <card-comp :btns="btnArray" class="mx-auto card-comp">
+          <form>
+            <div class="mb-3 mt-2">
+              <input
+                type="email"
+                class="form-control"
+                placeholder="Ingresa tu email"
+              />
+            </div>
+            <div class="mb-3 mt-2">
+              <input
+                type="password"
+                class="form-control"
+                placeholder="Contraseña"
+              />
+            </div>
+          </form>
+        </card-comp>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CardComp from "@/components/CardComp.vue";
+export default {
+  name: "HomeView",
+  components: {
+    CardComp,
+  },
+  data() {
+    return {
+      btnArray: [
+        {
+          title: "Iniciar Sesión",
+          txt: "Iniciar sesión",
+          class: "btn-primary",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+/*  */
+.home {
+  max-width: 895px;
+  margin: auto;
+  
+}
+.img-cont {
+  display: flex;
+  flex-direction: column;
+}
+img {
+  max-width: 315px;
+  margin: 1em auto 0 auto;
+}
+.card-comp {
+  max-width: 398px;
+  margin-top: 2.6em;
+}
+@media (min-width: 992px) {
+  .home {
+    margin: 4em auto;
+  }
+  img {
+    margin: 0;
+    margin-top: 5.6em;
+  }
+  h2 {
+    margin: 0;
+    margin-top: -0.8em;
+    margin-left: 0.8em;
+  }
+}
+</style>
